80cd2ca8291e435c05ea35b75c36ef5153bd2d25
George Mbugua <gmbugua2020@gmail.com>
2021-09-06 21:33:18 -0700

    prisma initialized and connected to sqlite db

--- src/index.js
+++ src/index.js
@@ -1,86 +1,79 @@
-const { ApolloServer } = require('apollo-server');
-const fs = require('fs');
-const path = require('path');
+/**
+ * Workflow
+ * --------
+ * Manually adjust your Prisma data model.
+ * Migrate your database using the prisma migrate CLI commands we covered. (Re-)generate Prisma Client
+ * Use Prisma Client in your application code to access your database.
+ */
+const { PrismaClient } = require("@prisma/client");
+const { ApolloServer } = require("apollo-server");
+
+const fs = require("fs");
+const path = require("path");
+
+const prisma = new PrismaClient()
 
 /**
  * @typeDefs define the GraphQL schema
  * note: info can never be null
  */
 const typeDefs = fs.readFileSync(
-  path.join(__dirname, 'schema.graphql'),
-  'utf8'
-)
+  path.join(__dirname, "schema.graphql"),
+  "utf8"
+);
 
 /**
  * @resolvers implementation of the GraphQl Schema
  */
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-const findLink = (id) => links.find(link => link.id == id)
-
-/**
- * 
- * @param {*} arg - represents an argument passed into updateLink
- * @param {*} comparator - used to determine if the arg is unique
- * @returns true if arg exists and does not equal comparator
- */
-const validUpdateArgument = (arg, comparator) => {
-  if (arg && arg !== comparator) 
-    return true
-  return false
-}
-
-/**
- * 
- * @param {*} linkId - id of a Link 
- * @returns index of a link given its id
- */
-const getlinkIndex = (linkId) => Number(linkId.split("-")[1]); 
-
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => findLink(args.id)
+    feed: async (parent, args, context, info) => {
+      return await context.prisma.link.findMany()
+    },
+    link: async (parent, args, context, info) => {
+      return await context.prisma.link.findUnique({
+        where: { 
+          id: Number(args.id)
+        }
+      })
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-      links.push(link)
-      return link
+    post: async (parent, args, context, info) => {
+      const newLink = await context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
     },
-    updateLink: (parent, args ) => {
-      const linkIndex = getlinkIndex(args.id)
-      if (validUpdateArgument(args.description, links[linkIndex].description))
-        links[linkIndex].description = args.description
-      if (validUpdateArgument(args.url, links[linkIndex].url))
-        links[linkIndex].url = args.url
-      return links[linkIndex]
+    updateLink: async (parent, args, context, info) => {
+      const updatedLink = await context.prisma.link.update({
+        where: {
+          id: args.id
+        },
+        data: {
+          url: args.url,
+          description: args.description
+        }
+      })
+      return updatedLink
     },
-    deleteLink: (parent, args) => links.splice(getlinkIndex(args.id))[0]
-  }
-}
+    deleteLink: (parent, args, context, info) => {
+      const deletedLink = await context.prisma.link.delete({
+        where: {
+          id: args.id
+        }
+      })
+    },
+  },
+};
 
-/**
- * @typeDefs and @resolvers are bundled to inform server 
- * of what API ops are accepted and resolved. 
- */
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-})
+  context: {prisma},
+});
 
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
